985d8409e92ff1d4d49df78ce0f3616715f3cb8e
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-02-25 13:19:54 +0900

    shopに色々プロパティ付け足した

--- app/src/main/java/com/solt9029/soumeikaigourmet/androidsoumeikaigourmet/Shop.java
+++ app/src/main/java/com/solt9029/soumeikaigourmet/androidsoumeikaigourmet/Shop.java
@@ -2,13 +2,78 @@ package com.solt9029.soumeikaigourmet.androidsoumeikaigourmet;
 
 public class Shop {
     private String name;
+    private String category;
+    private String phone_number;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private String link;
+    private String comment;
+    private String owner_name;
+    private String owner_club;
+    private String owner_graduated_at;
+    private String owner_group;
 
-    public Shop(String name) {
-        super();
+    public Shop(String name, String category, String phone_number, String address, double latitude, double longitude, String link, String comment, String owner_name, String owner_club, String owner_graduated_at, String owner_group) {
         this.name = name;
+        this.category = category;
+        this.phone_number = phone_number;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.link = link;
+        this.comment = comment;
+        this.owner_name = owner_name;
+        this.owner_club = owner_club;
+        this.owner_graduated_at = owner_graduated_at;
+        this.owner_group = owner_group;
     }
 
     public String getName() {
-        return this.name;
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getOwner_name() {
+        return owner_name;
+    }
+
+    public String getOwner_club() {
+        return owner_club;
+    }
+
+    public String getOwner_graduated_at() {
+        return owner_graduated_at;
+    }
+
+    public String getOwner_group() {
+        return owner_group;
     }
 }
