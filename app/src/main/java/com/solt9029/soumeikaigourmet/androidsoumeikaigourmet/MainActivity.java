2337154707612dae3b48dc5ff714ddf56d727186
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-02-26 11:57:50 +0900

    dont ask againにチェックが入ってdenyされたときに無限ループになるバグを修正しました

--- app/src/main/java/com/solt9029/soumeikaigourmet/androidsoumeikaigourmet/MainActivity.java
+++ app/src/main/java/com/solt9029/soumeikaigourmet/androidsoumeikaigourmet/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -236,7 +237,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     // アプリに対する情報提供を許可してくれるかどうかのダイアログに対する返答を受け取った時
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        // if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) の条件をつけると、位置情報に関することだとわかる
+        // don't ask againにチェックが入ってdenyされたときに、無限ループに陥らないようにする記述です
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            return;
+        }
         startLocationUpdates();
     }
 
