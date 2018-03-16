2e0330e79882225ad43fdb4467b73c84a9666075
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-03-16 18:03:18 +0900

    serialize追加した

--- app/src/main/java/com/solt9029/soumeikaigourmet/androidsoumeikaigourmet/Shop.java
+++ app/src/main/java/com/solt9029/soumeikaigourmet/androidsoumeikaigourmet/Shop.java
@@ -1,18 +1,45 @@
 package com.solt9029.soumeikaigourmet.androidsoumeikaigourmet;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Shop {
+    @SerializedName("id")
     private int id;
+
+    @SerializedName("name")
     private String name;
+
+    @SerializedName("category")
     private String category;
+
+    @SerializedName("phone_number")
     private String phoneNumber;
+
+    @SerializedName("address")
     private String address;
+
+    @SerializedName("latitude")
     private double latitude;
+
+    @SerializedName("longitude")
     private double longitude;
+
+    @SerializedName("link")
     private String link;
+
+    @SerializedName("comment")
     private String comment;
+
+    @SerializedName("owner_name")
     private String ownerName;
+
+    @SerializedName("owner_club")
     private String ownerClub;
+
+    @SerializedName("owner_graduated_at")
     private String ownerGraduatedAt;
+
+    @SerializedName("owner_group")
     private String ownerGroup;
 
     public Shop(int id, String name, String category, String phoneNumber, String address, double latitude, double longitude, String link, String comment, String ownerName, String ownerClub, String ownerGraduatedAt, String ownerGroup) {
